f4469d949e6ef9a72cc90f113d4c98fe67810bcf
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-11-21 22:09:06 +0100

    Restaurant Finder

---
+++ components/home/GoogleMapView.jsx
@@ -0,0 +1,60 @@
+import { UserLocationContext } from '@/context/UserLocationContext'
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api'
+import React, { useContext, useEffect, useState } from 'react'
+import Markers from './Markers'
+import { SelectedBusinessContext } from '@/context/SelectedBusinessContext'
+
+function GoogleMapView({ businessList }) {
+  const { userLocation, setUserLocation } = useContext(UserLocationContext)
+  const { selectedBusiness, setSelectedBusiness } = useContext(SelectedBusinessContext)
+  const [map, setMap] = useState();
+
+  const containerStyle = {
+    width: '100%',
+    height: '500px',
+
+  }
+
+  useEffect(() => {
+    if (map && selectedBusiness) {
+      map.panTo(selectedBusiness.geometry.location);
+
+    }
+  }, [selectedBusiness])
+  return (
+    <div>
+      <LoadScript
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        mapIds={['24f7ccecda4d78e7']}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          // center={userLocation}
+
+          center={
+            !selectedBusiness.name ? userLocation : selectedBusiness.geometry.location
+          }
+          options={{ mapId: '24f7ccecda4d78e7' }}
+          zoom={13}
+          onLoad={map => setMap(map)}
+        >
+          <MarkerF
+            position={userLocation}
+            icon={{
+              url: '/user-location.png',
+              scaledSize: {
+                width: 50,
+                height: 50
+              }
+            }}
+          />
+          {businessList.map((item, index) => index <= 7 && (
+            <Markers business={item} key={index} />
+          ))}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  )
+}
+
+export default GoogleMapView
